dddd4a4e820a6bc725e9871d3389984bbd4f9778
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-06 22:16:32 +0530

    Create 11_StringBuilder.cs

---
+++ 11_StringBuilder.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+namespace stringBuild{
+  class Program{
+    static void Main(String[] args){
+      StringBuilder sd = new StringBuilder();
+      sd.Append("hello");
+      sd.Append("my name is tushar");
+      System.Console.WriteLine(sd.ToString());
+      sd.Clear();
+      System.Console.WriteLine("this: "+sd.ToString());
+      
+      sd.AppendLine("hello world!");
+      sd.AppendLine("my name is tushar!");
+      System.Console.WriteLine(sd.ToString());
+      
+      StringBuilder sd1 = new StringBuilder("this is sd1.");
+      System.Console.WriteLine(sd1.ToString());
+    }
+  }
+}
