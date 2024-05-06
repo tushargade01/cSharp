37fd1f6c529615e036410ee9ba6610ae31f8cc5d
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-06 20:02:16 +0530

    Create 06_chars.cs

---
+++ 06_chars.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace exampleChar
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		  char ch1 = 'a';
+		  char ch2 = '\n';
+		  System.Console.WriteLine("hello: ");
+			Console.WriteLine(ch2);//add new line sing ch2
+			System.Console.WriteLine(ch1);
+		}
+	}
+}
