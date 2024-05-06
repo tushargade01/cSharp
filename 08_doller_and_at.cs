649500cf79c41c2429f8ee47da546ea013fbdb9b
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-06 20:22:38 +0530

    Create 08_doller_and_at.cs

---
+++ 08_doller_and_at.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Program1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		  string @if = "Tushar"; //add @ to prefix for use build in keywords
+			
+			System.Console.WriteLine($"hello, my name is {@if}.");
+		}
+	}
+}
