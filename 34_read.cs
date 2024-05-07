762164bc9fc871b970344cd9a32585abcc6bab4a
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 19:42:33 +0530

    Create 34_read.cs

---
+++ 34_read.cs
@@ -0,0 +1,14 @@
+using System;
+namespace firstProgram {
+    public class Program 
+    {
+        public static void Main(String[] args) {
+            System.Console.WriteLine("enter first number: ");
+            int a = Console.Read();
+            System.Console.WriteLine(a);
+            //int num = Convert.ToInt32(a);
+            System.Console.WriteLine(Convert.ToChar(a));
+
+        }
+    }
+}
