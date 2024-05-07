3c683497cb519d16f436e11ce8da03d2e96d2ebd
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 18:01:11 +0530

    Create 31_infiniteLoop.cs

---
+++ 31_infiniteLoop.cs
@@ -0,0 +1,18 @@
+//dont run
+namespace infiniteLoop
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		  /*
+			for( ; ; ){
+			  System.Console.WriteLine("hello");
+			}
+			*/
+			while(true){
+			  System.Console.WriteLine("Wrold!"); //its going to infinite Loop
+			}
+		}
+	}
+}
