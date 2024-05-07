934884bcaa8665a21018d4888791860a65f38459
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 17:42:41 +0530

    Create 28_whileLoop.cs

---
+++ 28_whileLoop.cs
@@ -0,0 +1,14 @@
+namespace whileLoop
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		  int i=1;
+		  while(i<=10){
+		    System.Console.WriteLine(i);
+		    i++;
+		  }
+		}
+	}
+}
