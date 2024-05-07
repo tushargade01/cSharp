f8847992d36b27a0073610dd368de01912fc6db1
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 06:41:41 +0530

    Create 14_treeBoolValue.cs

---
+++ 14_treeBoolValue.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ThreeBoolValue
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			bool? tushar = null;
+			bool isComplete = false;
+			bool notComplete = true;
+			System.Console.WriteLine(tushar); //bool has three value true,false,null using (boo?).
+		}
+	}
+}
