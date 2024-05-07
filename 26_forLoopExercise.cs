2369e2d6387a2ff372aafc6e2c5275f67a10a28d
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 17:30:25 +0530

    Create 26_forLoopExercise.cs

---
+++ 26_forLoopExercise.cs
@@ -0,0 +1,15 @@
+using System;
+namespace forLoopExercise
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			int x = 0;
+			for(int i=0;i<100;i++){
+			  x+=i;
+			}
+			System.Console.WriteLine(x);
+		}
+	}
+}
