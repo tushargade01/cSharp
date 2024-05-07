b9a960c42546ee3821721de31b0f53923646d462
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 17:46:16 +0530

    Create 29_whileLoopExercise.cs

---
+++ 29_whileLoopExercise.cs
@@ -0,0 +1,14 @@
+namespace whileLoopExercise
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		  int x = 3;
+		  while(x>0){
+		    System.Console.WriteLine(x);
+		    x--;
+		  }
+		}
+	}
+}
