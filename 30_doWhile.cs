998f9c34f0e88a98841285417ae91749be61ec9e
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 17:54:06 +0530

    Create 30_doWhile.cs

---
+++ 30_doWhile.cs
@@ -0,0 +1,12 @@
+using System;
+namespace doWhile{
+  class Program{
+    public static void Main(String[] args){
+      int i = 0;
+      do{
+        System.Console.WriteLine(i);
+        i++;
+      }while(i<=5);
+    }
+  }
+}
