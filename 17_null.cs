2a0aac7093524352558d9ec19a979221d677503e
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 07:16:48 +0530

    Create 17_null.cs

---
+++ 17_null.cs
@@ -0,0 +1,13 @@
+namespace Null
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			object o = null; //object is refernce type
+			//int i = null; we not sign null like this
+			int? i = null;
+			System.Console.WriteLine(i);
+		}
+	}
+}
