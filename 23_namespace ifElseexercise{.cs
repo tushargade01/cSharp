83243c73e8b240b8d7313a08ab26c2d072906eb5
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2024-05-07 15:04:00 +0530

    Create 23_namespace ifElseexercise{.cs

---
+++ 23_namespace ifElseexercise{.cs
@@ -0,0 +1,18 @@
+using System;
+namespace ifElseexercise{
+  public static class exercise{
+    public static void Main(String[] args){
+      bool condition = true;
+      int x = 0;
+      if (condition)
+      {
+        x = 5;
+      }
+      else
+      {
+        x = 3;
+      }
+      System.Console.WriteLine(x);
+    }
+  }
+}
